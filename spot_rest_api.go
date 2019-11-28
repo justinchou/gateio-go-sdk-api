6ce4dae6d5c4a6df57181afd12cf93b17f0183d7
Justin <zhou78620051@126.com>
2019-11-28 17:43:28 +0800

    add: struct for api response, and test

--- spot_rest_api.go
+++ spot_rest_api.go
@@ -1,75 +1,326 @@
 package gateio
 
-// GetPairs all support pairs
-func (client *Client) GetPairs() string {
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// CoinPairs 交易对列表
+type CoinPairs []string
+
+// GetPairs 获取所有交易对列表
+func (client *Client) GetPairs() (coinpairs *CoinPairs, err error) {
 	method := "GET"
 	url := client.Config.PublicEndpoint + "api2/1/pairs"
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	coinpairs = &CoinPairs{}
+	err = json.Unmarshal([]byte(ret), coinpairs)
+	if err != nil {
+		return nil, err
+	}
+
+	if client.Config.IsPrint {
+		fmt.Println("coinpairs", coinpairs)
+	}
+
+	return coinpairs, nil
+}
+
+type _MarketInfoResp struct {
+	Result string                   `json:"result"`
+	Pairs  []map[string]*MarketInfo `json:"pairs"`
+}
+
+// MarketInfo 交易对基本信息
+type MarketInfo struct {
+	DecimalPlaces int     `json:"decimal_places"`
+	MinAmount     float64 `json:"min_amount"`
+	MinAmountA    float64 `json:"min_amount_a"`
+	MinAmountB    float64 `json:"min_amount_b"`
+	Fee           float64 `json:"fee"`
+	TradeDisabled int     `json:"trade_disabled"`
+	Symbol        string  `json:"symbol"`
 }
 
 // MarketInfo Market Info
-func (client *Client) MarketInfo() string {
+func (client *Client) MarketInfo() (marketInfos *[]*MarketInfo, err error) {
 	method := "GET"
 	url := client.Config.PublicEndpoint + "api2/1/marketinfo"
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	marketInfoResp := &_MarketInfoResp{}
+	err = json.Unmarshal([]byte(ret), marketInfoResp)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	marketInfoList := []*MarketInfo{}
+	for _, info := range marketInfoResp.Pairs {
+		for k, v := range info {
+			v.Symbol = k
+			marketInfoList = append(marketInfoList, v)
+		}
+	}
+
+	return &marketInfoList, nil
+}
+
+type _MarketListResp struct {
+	Result string        `json:"result"`
+	Data   *[]MarketItem `json:"data"`
+}
+
+// MarketItem 交易对信息
+type MarketItem struct {
+	No          int         `json:"no"`
+	Symbol      string      `json:"symbol"`
+	Name        string      `json:"name"`
+	NameEn      string      `json:"name_en"`
+	NameCn      string      `json:"name_cn"`
+	Pair        string      `json:"pair"`
+	Rate        string      `json:"rate"`
+	VolA        string      `json:"vol_a"`
+	VolB        string      `json:"vol_b"`
+	CurrA       string      `json:"curr_a"`
+	CurrB       string      `json:"curr_b"`
+	CurrSuffix  string      `json:"curr_suffix"`
+	RatePercent string      `json:"rate_percent"`
+	Trend       string      `json:"trend"`
+	Supply      int         `json:"supply"`
+	Marketcap   interface{} `json:"marketcap"`
+	Lq          string      `json:"lq"`
 }
 
 // MarketList Market Details
-func (client *Client) MarketList() string {
+func (client *Client) MarketList() (marketList *[]MarketItem, err error) {
 	method := "GET"
 	url := client.Config.PublicEndpoint + "api2/1/marketlist"
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	marketListResp := &_MarketListResp{}
+	err = json.Unmarshal([]byte(ret), marketListResp)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	marketList = marketListResp.Data
+
+	return marketList, nil
+}
+
+// TickerInfo Ticker 数据
+type TickerInfo struct {
+	Result        string `json:"result"`
+	Last          string `json:"last"`
+	LowestAsk     string `json:"lowestAsk"`
+	HighestBid    string `json:"highestBid"`
+	PercentChange string `json:"percentChange"`
+	BaseVolume    string `json:"baseVolume"`
+	QuoteVolume   string `json:"quoteVolume"`
+	High24Hr      string `json:"high24hr"`
+	Low24Hr       string `json:"low24hr"`
+	Elapsed       string `json:"elapsed"`
 }
 
 // Tickers tickers
-func (client *Client) Tickers() string {
+func (client *Client) Tickers() (tickerInfos *map[string]*TickerInfo, err error) {
 	method := "GET"
 	url := client.Config.PublicEndpoint + "api2/1/tickers"
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	tickerInfos = &map[string]*TickerInfo{}
+	err = json.Unmarshal([]byte(ret), tickerInfos)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	return tickerInfos, nil
 }
 
 // Ticker ticker
-func (client *Client) Ticker(ticker string) string {
+func (client *Client) Ticker(symbol string) (tickerInfo *TickerInfo, err error) {
 	method := "GET"
-	url := client.Config.PublicEndpoint + "api2/1/ticker" + "/" + ticker
+	url := client.Config.PublicEndpoint + "api2/1/ticker" + "/" + symbol
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+	fmt.Println(ret)
+
+	tickerInfo = &TickerInfo{}
+	err = json.Unmarshal([]byte(ret), tickerInfo)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	return tickerInfo, nil
+}
+
+type _OrderBookItem struct {
+	Result string      `json:"result"`
+	Asks   [][2]string `json:"asks"`
+	Bids   [][2]string `json:"bids"`
+}
+
+// Ask 卖
+type Ask struct {
+	Price  string `json:"price"`
+	Amount string `json:"amount"`
+}
+
+// Bid 买
+type Bid struct {
+	Price  string `json:"price"`
+	Amount string `json:"amount"`
+}
+
+// OrderBook 挂单列表
+type OrderBook struct {
+	Asks    []Ask  `json:"asks"`
+	Bids    []Bid  `json:"bids"`
+	Elapsed string `json:"elapsed"`
 }
 
 // OrderBooks Depth
-func (client *Client) OrderBooks() string {
+func (client *Client) OrderBooks() (retItems map[string]*OrderBook, err error) {
 	method := "GET"
 	url := client.Config.PublicEndpoint + "api2/1/orderBooks"
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	orderbooksResp := map[string]*_OrderBookItem{}
+	err = json.Unmarshal([]byte(ret), &orderbooksResp)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	orderbooks := map[string]*OrderBook{}
+	for symbol, orderbook := range orderbooksResp {
+		orderbooks[symbol] = &OrderBook{}
+
+		orderbooks[symbol].Asks = []Ask{}
+		for _, ask := range orderbook.Asks {
+			orderbooks[symbol].Asks = append(
+				[]Ask{
+					Ask{ask[0], ask[1]},
+				},
+				orderbooks[symbol].Asks...,
+			)
+		}
+
+		orderbooks[symbol].Bids = []Bid{}
+		for _, bid := range orderbook.Bids {
+			orderbooks[symbol].Bids = append(
+				orderbooks[symbol].Bids,
+				Bid{bid[0], bid[1]},
+			)
+		}
+	}
+
+	return orderbooks, nil
+}
+
+type _OrderbookResp struct {
+	Asks    [][2]string `json:"asks"`
+	Bids    [][2]string `json:"bids"`
+	Elapsed string      `json:"elapsed"`
 }
 
 // OrderBook Depth of pair
-func (client *Client) OrderBook(params string) string {
+func (client *Client) OrderBook(symbol string) (orderbooks *OrderBook, err error) {
 	method := "GET"
-	url := client.Config.PublicEndpoint + "api2/1/orderBook/" + params
+	url := client.Config.PublicEndpoint + "api2/1/orderBook/" + symbol
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	orderbooksResp := &_OrderbookResp{}
+	err = json.Unmarshal([]byte(ret), orderbooksResp)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	orderbooks = &OrderBook{}
+
+	orderbooks.Asks = []Ask{}
+	for _, ask := range orderbooksResp.Asks {
+		orderbooks.Asks = append(
+			[]Ask{
+				Ask{ask[0], ask[1]},
+			},
+			orderbooks.Asks...,
+		)
+	}
+
+	orderbooks.Bids = []Bid{}
+	for _, bid := range orderbooksResp.Bids {
+		orderbooks.Bids = append(
+			orderbooks.Bids,
+			Bid{bid[0], bid[1]},
+		)
+	}
+
+	orderbooks.Elapsed = orderbooksResp.Elapsed
+
+	return orderbooks, nil
+}
+
+// TradeHistory 成交历史记录
+type TradeHistory struct {
+	TradeID   string `json:"tradeID"`
+	Total     string `json:"total"`
+	Date      string `json:"date"`
+	Rate      string `json:"rate"`
+	Amount    string `json:"amount"`
+	Timestamp string `json:"timestamp"`
+	Type      string `json:"type"`
+}
+
+type _TradeHistoryResp struct {
+	Data    []TradeHistory `json:"data"`
+	Result  string         `json:"result"`
+	Elapsed string         `json:"elapsed"`
 }
 
 // TradeHistory Trade History
-func (client *Client) TradeHistory(params string) string {
+func (client *Client) TradeHistory(symbol string) (history *[]TradeHistory, err error) {
 	method := "GET"
-	url := client.Config.PublicEndpoint + "api2/1/tradeHistory/" + params
+	url := client.Config.PublicEndpoint + "api2/1/tradeHistory/" + symbol
 	param := ""
 	ret := client.Request(method, url, param)
-	return ret
+
+	resp := &_TradeHistoryResp{}
+	err = json.Unmarshal([]byte(ret), resp)
+	if err != nil {
+		if client.Config.IsPrint {
+			fmt.Println("parse json err", err)
+		}
+		return nil, err
+	}
+
+	history = &resp.Data
+
+	return history, nil
 }
 
 // Balances Get account fund balances
--- spot_rest_api_test.go
+++ spot_rest_api_test.go
@@ -1,16 +1,95 @@
 package gateio
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestGetPairs(t *testing.T) {
 	c := NewTestClient()
-	ac := c.GetPairs()
-
-	fmt.Printf("%+v", ac)
+	ac, _ := c.GetPairs()
 
 	jstr, _ := Struct2JsonString(ac)
-	println(jstr)
+	t.Log(jstr)
+}
+
+func TestMarketInfo(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.MarketInfo()
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
+}
+
+func TestMarketList(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.MarketList()
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
+}
+
+func TestTickers(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.Tickers()
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
+}
+
+func TestTicker(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.Ticker("btc_usdt")
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
+}
+
+func TestOrderBooks(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.OrderBooks()
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
+}
+
+func TestOrderBook(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.OrderBook("btc_usdt")
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
+}
+
+func TestTradeHistory(t *testing.T) {
+	c := NewTestClient()
+	ac, err := c.TradeHistory("btc_usdt")
+
+	if err != nil {
+		t.Error("invalid json parse")
+	}
+
+	jstr, _ := Struct2JsonString(ac)
+	t.Log(jstr)
 }
